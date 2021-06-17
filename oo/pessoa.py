feccd1cddffb36e0bc7fe65b486617a2cdffd66c
Zumpiatti <lucaschristianobz@hotmail.com>
2021-06-17 20:09:36 -0300

    Metodo criado

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,10 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Hello There. GENERAL KENOBI {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
