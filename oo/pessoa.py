07ec51cda046c57488b0ca6f454ecf2295bb9f8f
Zumpiatti <lucaschristianobz@hotmail.com>
2021-06-20 20:47:30 -0300

    Atributos de instancia, nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=25):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Hello There. GENERAL KENOBI {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Grievous')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'KENOBI'
+    print(p.nome)
+    print(p.idade)
